97a8639751723730d474f37ccb5f2ef616a97e86
Sender Flores <safcrace@gmail.com>
2016-12-10 11:55:41 -0600

    integrated chart-js

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -28,25 +28,50 @@ var data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
         {
-            label: "My First dataset",
+            label: "Entradas",
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1,
+            data: [65, 59, 80, 81, 56, 55, 50],
+        },
+        {
+            label: "Salidas",
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)'
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)'
             ],
             borderWidth: 1,
-            data: [65, 59, 80, 81, 56, 55, 40],
+            data: [50, 25, 40, 51, 46, 35, 60],
         }
     ]
 };
@@ -55,3 +80,103 @@ var myBarChart = new Chart(ctx, {
     type: 'bar',
     data: data
   });
+
+  var ctxs = document.getElementById("myChartSemanal").getContext("2d");
+
+
+  var data = {
+      labels: ["Semana1", "Semana2", "Semana3", "Semana4"],
+      datasets: [
+          {
+              label: "Entradas",
+              backgroundColor: [
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(75, 192, 192, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(75, 192, 192, 1)'
+              ],
+              borderWidth: 1,
+              data: [48, 34, 55, 62],
+          },
+          {
+              label: "Salidas",
+              backgroundColor: [
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(255, 206, 86, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(255, 206, 86, 1)'
+              ],
+              borderWidth: 1,
+              data: [51, 43, 40, 47],
+          }
+      ]
+  };
+
+  var myBarChartSemanal = new Chart(ctxs, {
+      type: 'bar',
+      data: data
+    });
+
+  var ctxd = document.getElementById("myChartDiario").getContext("2d");
+
+
+  var data = {
+      labels: ["Lun", "Mar", "Mie", "Jue", "Vie"],
+      datasets: [
+          {
+              label: "Entradas",
+              backgroundColor: [
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(153, 102, 255, 1)'
+              ],
+              borderWidth: 1,
+              data: [25, 20, 36, 20, 18],
+          },
+          {
+              label: "Salidas",
+              backgroundColor: [
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1,
+              data: [18, 25, 35, 17, 37],
+          }
+      ]
+  };
+
+  var myBarChartDiario = new Chart(ctxd, {
+      type: 'bar',
+      data: data
+    });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,21 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
-                <div class="container">
+                <div class="container"  style="width: 40%">
+                    <h3>Informe de Entradas/Salidas por Dia</h3>
+                    <canvas id="myChartDiario" width="600" height="400"></canvas>
+                </div>
+                <hr>
+                <div class="container"  style="width: 40%">
+                    <h3>Entradas/Salidas por Semana</h3>
+                    <canvas id="myChartSemanal" width="600" height="400"></canvas>
+                </div>
+                <hr>
+                <div class="container"  style="width: 40%">
+                    <h3>Entradas/Salidas por Mes</h3>
                     <canvas id="myChart" width="600" height="400"></canvas>
                 </div>
+
             </div>
         </div>
     </div>
